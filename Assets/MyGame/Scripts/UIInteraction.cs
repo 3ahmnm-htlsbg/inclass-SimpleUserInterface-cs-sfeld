a214016c37ab791fb03ad97531b74cedd6c9cbf6
feldbacherSimone <simone.feldbacher03@htl-salzburg.ac.at>
2020-01-07 14:13:57 +0100

    create UI Script and adit it to empty gameObject

---
+++ Assets/MyGame/Scripts/UIInteraction.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIInteraction : MonoBehaviour
+{
+    public InputField textInput;
+    public Button textButton;
+    public 
+   
+}
